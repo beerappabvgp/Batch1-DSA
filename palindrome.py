ddae667456ad8faf445653bb3389273ca4a249af
beerappabvgp <beerappabharathb@gmail.com>
2025-01-02 21:38:10 +0530

    pointer approach palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,5 @@
+# First solution by vamsi
+
 def palindrome(s):
     reverse = "" # len(s)
     for i in range(len(s) - 1, -1, -1): # 0(len(s))  
@@ -12,12 +14,34 @@ def palindrome(s):
 
 s = ""
 
-print(palindrome(s))
+# print(palindrome(s))
 
 # i/p space - infinity
 
 # o/p space - True/False
 
-# tc - O(len(s))
+# tc - O(len(s)) // 6
 # sc - O(len(s))
 
+
+# second solution by tahir
+s = "madam"
+
+def palindrome(s):
+    left = 0   # O(1)
+    right = len(s) - 1 # O(1)
+    while left < right:    # O(len(s)) // 2 3 
+        if s[left] == s[right]: # O(1) 2    
+            left += 1 # O(1)
+            right -= 1  # O(1)
+        else:
+            return False
+    return True
+
+    # overall time complexity - O(len(s)) // 2 * O(1) + O(1) + O(1)
+    # O(len(s)) // 2 - 6 // 2 = 3
+
+print(palindrome(s))
+
+# O(len(s))
+# O(1)
